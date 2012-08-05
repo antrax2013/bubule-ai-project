c18517fc47536d422655682377b549c8c77d16f3
cyrilcophignon@yahoo.fr <cyrilcophignon@yahoo.fr@37683267-b89c-5703-4bd7-a4ad5d73b456>
2012-08-05 08:42:42 +0000

    Version release

--- branches/IA V0.1/library/engine/Besoins/BesoinNager.php
+++ branches/IA V0.1/library/engine/Besoins/BesoinNager.php
@@ -1,22 +1,34 @@
 <?php
+    require_once "engine/Abstract/AbstractBesoin.php";
 
-require_once "engine/Abstract/AbstractBesoin.php";
-
+/**
+ * Classe BesoinNager, hérite de la classe abstraite ABesoin
+ * Dans cette version de base nager consomme de l'ernergie.
+ * Pas de notion de déplacement ici.
+ * @class classe définissant l'action de nager
+ * @date 03/08/2012
+ * @autor Cyril Cophignon
+ * @extends ABesoin
+ */
 class BesoinNager extends ABesoin
 {
 
-    public function Check($a_energie, $a_energieMax)
+    /**
+     * Méthode gérant l'activation/désactivations en fonction de l'état des paramètres
+     * @param int $a_param parametre
+     * @param int $a_paramRef parametre de référence
+     * @return bool renvoi true si le besoin est actif ou false
+     */
+    public function Check($a_param, $a_paramRef)
     {
-        if($a_energie ==  $a_energieMax && !$this->active)
+        if($a_param ==  $a_paramRef && !$this->active)
         {
-            //echo "Nager activer";
             $this->Activer();
             return true;
         }
 
-        if($a_energie <= 0 && $this->active)
+        if($a_param <= 0 && $this->active)
         {
-            //echo "Nager désactiver";
             $this->Assouvi();
             return false;
         }
